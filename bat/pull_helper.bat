fa9bf48266a43a9341204570bf9e28d4af519101
TONY） <govovo12@gmail.com>
2025-06-16 03:36:20 +0800

    修正bat

--- bat/pull_helper.bat
+++ bat/pull_helper.bat
@@ -1,20 +1,26 @@
 @echo off
 echo [SYNC] Resetting local branch to match remote...
 
+:: 切回專案根目錄（bat 上層）
+cd /d "%~dp0.."
+
 :: Get current branch name
 for /f %%i in ('git branch --show-current') do set CUR_BRANCH=%%i
 
 :: Reset to remote branch
 git fetch origin
 git reset --hard origin/%CUR_BRANCH%
-
-:: Clean all untracked files including those in .gitignore
 git clean -fdx
 
-:: Extra: Force remove venv folder if exists
+:: Extra: try removing common venv folders
 if exist venv (
-    echo [CLEAN] Deleting local venv folder...
+    echo [CLEAN] Deleting venv folder...
     rmdir /s /q venv
+)
+
+if exist .venv (
+    echo [CLEAN] Deleting .venv folder...
+    rmdir /s /q .venv
 )
 
 echo [DONE] Local project is now reset to origin/%CUR_BRANCH%
